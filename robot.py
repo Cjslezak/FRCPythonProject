f8705dc678f03093a969f95427ecb34f21fe179a
slezakc <carl.j.slezak@leidos.com>
2025-01-13 10:13:46 -0500

    Initial Commit

---
+++ robot.py
@@ -0,0 +1,19 @@
+import wpilib
+import wpilib.drive
+import magicbot
+
+
+class MyRobot(magicbot.MagicRobot):
+    def createObjects(self):
+        # Create motors and stuff here
+        pass
+
+    def teleopInit(self):
+        # Called when teleop starts; optional
+        pass
+
+    def teleopPeriodic(self):
+        # Called every 20ms when teleop runs
+        pass
+
+
